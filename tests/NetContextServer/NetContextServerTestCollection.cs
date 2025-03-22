a8e517ceef917ccd9dc0720640d714fb66ac2cf1
Brandon Williams <willibrandon@gmail.com>
2025-03-22 02:32:30 -0700

    Remove unused NetContextServerTestCollection class from tests directory

--- tests/NetContextServer/NetContextServerTestCollection.cs
+++
@@ -1,9 +0,0 @@
-namespace NetContextServer.Tests;
-
-[CollectionDefinition("NetContextServer Tests", DisableParallelization = true)]
-public class NetContextServerTestCollection : ICollectionFixture<NetContextServerTestCollection>
-{
-    // This class has no code, and is never created. Its purpose is simply
-    // to be the place to apply [CollectionDefinition] and all the
-    // ICollectionFixture<> interfaces.
-}
